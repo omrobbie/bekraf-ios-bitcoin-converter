bfeda8cf77717dc8e0dee9eef40bb5019ad65f7f
omrobbie <putu.roby@yahoo.com>
2017-07-19 21:08:15 +0800

    bikin dummy data untuk picker view

--- Bitcoin Converter/ViewController.swift
+++ Bitcoin Converter/ViewController.swift
@@ -8,18 +8,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource  {
+    
+    var countryData: [String] = [String]()
 
+    @IBOutlet weak var pvCountry: UIPickerView!
+    
+    @IBAction func btnCountry(_ sender: UIButton) {
+        pvCountry.isHidden = !pvCountry.isHidden
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // connect to picker view
+        self.pvCountry.delegate = self
+        self.pvCountry.dataSource = self
+        
+        // create dummy picker data
+        countryData = ["Indonesian Rupiah", "United States Dollar", "Japanese Yen", "Malaysian Ringgit", "Singapore Dollar", "Australian Dollar"]
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    // picker view
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return countryData.count;
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return countryData[row] as String
+    }
+    //---
+    
 }
 
